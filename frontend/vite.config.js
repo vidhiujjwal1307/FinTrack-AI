ba860c991c14db1f8e85e9192a025eecb8acf72b
yashvardhan dhaka <yashvardhandhaka777@gmail.com>
2026-06-13 11:13:30 +0530

    loginsigupWorking

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  server: {
+    port: 5174,
+  },
   plugins: [
     react(),
     tailwindcss(),
