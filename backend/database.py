6ec656e54079411bc88cf5a77f63a7a45738fbeb
Your Name <vidhiujjwal07@gmail.com>
2026-06-18 23:24:24 +0530

    Fix CORS

--- backend/database.py
+++ backend/database.py
@@ -2,11 +2,11 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "sqlite:///./finance.db"
 
 engine = create_engine(
     DATABASE_URL,
-    pool_pre_ping=True
+    connect_args={"check_same_thread": False}
 )
 
 SessionLocal = sessionmaker(
